1e58f1e74b0292ef0fbcb7ec9146e76f6a2cba39
Josh Fried <joshuafried@gmail.com>
2018-09-12 17:09:36 -0400

    use older version of corearbiter that works

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -13,6 +13,13 @@ echo "Installing arachne-all to ${ARACHNE_DIR}"
 
 git clone --recursive https://github.com/PlatformLab/arachne-all.git ${ARACHNE_DIR}
 pushd arachne-all
+
+# Use working version
+cd CoreArbiter
+git checkout 9bd2ddbd14cadd08a70c7d159fc95882dce4d680
+git am ${MEMCACHED_DIR}/CoreArbiter.patch
+cd ../
+
 ./buildAll.sh
 popd
 
